3c21c85edf9202105574917410304c40b08be079
christian <c41850211@gmail.com>
2024-12-01 19:09:34 +0100

    ntend

---
+++ index.tsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import type { NextPage } from "next";
+import { useWallet } from '@meshsdk/react';
+import { CardanoWallet } from '@meshsdk/react';
+
+const Home: NextPage = () => {
+  const { connected, wallet } = useWallet();
+  const [assets, setAssets] = useState<null | any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function votar( voto: String) {
+    if (wallet) {
+
+
+
+
+
+
+      const dRep = await wallet.getDRep();
+      const dRepId = dRep.dRepIDCip105;
+      
+      const utxos = await wallet.getUtxos();
+      const changeAddress = await wallet.getChangeAddress();
+      
+      const txBuilder = getTxBuilder();
+      txBuilder
+        .vote(
+          {
+            type: "DRep",
+            drepId: dRepId,
+          },
+          {
+            txHash: 'aff2909f8175ee02a8c1bf96ff516685d25bf0c6b95aac91f4dfd53a5c0867cc',
+            txIndex: 0,
+          },
+          {
+            voteKind: voto,
+          },
+        )
+        .selectUtxosFrom(utxos)
+        .changeAddress(changeAddress);
+      
+      const unsignedTx = await txBuilder.complete();
+      const signedTx = await wallet.signTx(unsignedTx);
+      const txHash = await wallet.submitTx(signedTx);
+
+
+
+
+
+
+
+
+
+    }
+  }
+
+  return (
+    <div>
+      <h1>Connect Wallet</h1>
+      <CardanoWallet />
+      {connected && (
+        <>
+          <h1>Get Wallet Assets</h1>
+            <button
+              type="button"
+              onClick={() => votar("Yes")}
+              disabled={loading}
+              style={{
+                margin: "8px",
+                backgroundColor: loading ? "orange" : "grey",
+              }}
+            >
+              Votar SI
+            </button>
+            <button
+              type="button"
+              onClick={() => votar("No")}
+              disabled={loading}
+              style={{
+                margin: "8px",
+                backgroundColor: loading ? "orange" : "grey",
+              }}
+            >
+              Votar NO
+            </button>
+            <button
+              type="button"
+              onClick={() => votar("Abstend")}
+              disabled={loading}
+              style={{
+                margin: "8px",
+                backgroundColor: loading ? "orange" : "grey",
+              }}
+            >
+              Abstencion
+            </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
---
+++ register.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import type { NextPage } from "next";
+import { useWallet } from '@meshsdk/react';
+import { CardanoWallet } from '@meshsdk/react';
+import { keepRelevant, MeshTxBuilder, Quantity, Unit } from "@meshsdk/core";
+
+const Home: NextPage = () => {
+  const { connected, wallet } = useWallet();
+  const [assets, setAssets] = useState<null | any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  async function registrar() {
+    if (wallet) {
+
+
+        const dRep = await wallet.getDRep();
+        const dRepId = dRep.dRepIDCip105;
+        
+        const anchorUrl = 'https://github.com/tech-week-hackathon/hackathon-christian-camara/blob/main/plutus.json';
+        const anchorHash = await getMeshJsonHash(anchorUrl);
+        
+        // get utxo to pay for the registration
+        const utxos = await wallet.getUtxos();
+        const registrationFee = "500000000";
+        const assetMap = new Map<Unit, Quantity>();
+        assetMap.set("lovelace", registrationFee);
+        const selectedUtxos = keepRelevant(assetMap, utxos);
+        
+        const changeAddress = await wallet.getChangeAddress();
+        
+        txBuilder
+          .drepRegistrationCertificate(dRepId, {
+            anchorUrl: anchorUrl,
+            anchorDataHash: anchorHash,
+          })
+          .changeAddress(changeAddress)
+          .selectUtxosFrom(selectedUtxos);
+        
+        const unsignedTx = await txBuilder.complete();
+        const signedTx = await wallet.signTx(unsignedTx);
+        const txHash = await wallet.submitTx(signedTx);
+
+
+
+    }
+  }
+  return (
+    <div>
+      <h1>Connect Wallet</h1>
+      <CardanoWallet />
+      {connected && (
+        <>
+          <h1>Registro DREP</h1>
+            <button
+              type="button"
+              onClick={() => votosi()}
+              disabled={loading}
+              style={{
+                margin: "8px",
+                backgroundColor: loading ? "orange" : "grey",
+              }}
+            >
+              Registrar drep
+            </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
+
+function getMeshJsonHash(anchorUrl: string) {
+    throw new Error("Function not implemented.");
+}
